9d2a5cda876184d0a54ef5d0dba6a56961e340c0
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-04 12:47:22 +0200

    Função Decrescente

---
+++ ordem_decrescente.py
@@ -0,0 +1,7 @@
+# Funcao que ordena numeros de uma lista em ordem decrescente
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def ordem_decrescente():
+    print('Lista em ordem decrescente: ', lista.sort(reverse=True))
